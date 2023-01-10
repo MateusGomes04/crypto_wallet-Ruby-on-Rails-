1a5d4824d13bf68ba734164c7994eb63992370dc
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-10 12:39:15 -0300

    feat: add partials menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Development"
+        elsif Rails.env.production?
+            "production"
+        else
+            "test"
+        end
+end
 end
