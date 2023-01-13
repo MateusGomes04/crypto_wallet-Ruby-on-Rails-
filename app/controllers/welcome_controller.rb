2baaf0a9ed8116d854d06602afbb0a1bb574e80f
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-13 11:34:20 -0300

    feat: Using multi-language application at runtime

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+     cookies[:curso] = "curso de ruby on rails"
+     session[:curso] = "curso de ruby on rails"
+     
+
     @my_name = params[:name]
     @course = "Rails"
   end
