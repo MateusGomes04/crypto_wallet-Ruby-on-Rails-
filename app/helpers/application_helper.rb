669e92ab46e23f64e4c2e0ca87db39c05e6d83bb
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-13 10:43:29 -0300

    feat: adding I18n to translate the application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
-    def data_br(data_us)
-        data_us.strftime("%d/%m/%Y")
-    end
 
+        def locale(locale)
+            if I18n.locale == :en
+                "American English"
+            else
+                "Português do Brasil"
+            end  
+        end             
+    
+        def data_br(data_us)
+    
+            data_us.strftime("%d/%m/%Y")
+        end
+    
     def ambiente_rails
         if Rails.env.development?
             "Development"
@@ -11,5 +21,5 @@ module ApplicationHelper
         else
             "test"
         end
-end
+    end
 end
