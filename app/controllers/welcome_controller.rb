eb1cfe368b2a1de7b1e90d77620f4d69c6522320
MateusGomes04 <mateusgomesc04@gmail.com>
2023-01-16 12:25:34 -0300

    feat: Precompiling and isolating assets by controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,6 @@ class WelcomeController < ApplicationController
   def index
      cookies[:curso] = "curso de ruby on rails (cookies)"
      session[:curso] = "curso de ruby on rails (session)"
-     
 
     @my_name = params[:name]
     @course = "Rails"
